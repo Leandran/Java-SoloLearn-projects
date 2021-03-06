ec45e282458e250ed19f55b710c4e4c1ce5d554a
Leandran <41592532+Leandran@users.noreply.github.com>
2021-03-06 20:20:18 +0200

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,3 +1,15 @@
+//Time-Converter
+
+//The program takes the amount of days as input and then convert days to seconds.
+
+//Sample Input:
+//12 days
+
+//Sample Output:
+//1036800 seconds
+
+
+
 import java.util.Scanner;
 
 public class Program {
